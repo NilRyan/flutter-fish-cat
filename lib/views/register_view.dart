8606913b0b01e001bc5d97a520ff66e589b11710
Neil Ryan L <neilryanlipaod@gmail.com>
2022-03-26 21:44:23 +0800

    Refactor register_view
    Extracted form field widgets into own functions for better readability

--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -13,7 +13,7 @@ class _RegisterViewState extends State<RegisterView> {
   late final TextEditingController _password;
   late final TextEditingController _confirmPassword;
   late final TextEditingController _name;
-  late final TextEditingController _birthday;
+  DateTime? _birthday;
   bool _obscureText = true;
 
   @override
@@ -22,7 +22,6 @@ class _RegisterViewState extends State<RegisterView> {
     _password = TextEditingController();
     _confirmPassword = TextEditingController();
     _name = TextEditingController();
-    _birthday = TextEditingController();
     super.initState();
   }
 
@@ -30,6 +29,8 @@ class _RegisterViewState extends State<RegisterView> {
   void dispose() {
     _email.dispose();
     _password.dispose();
+    _confirmPassword.dispose();
+    _name.dispose();
     super.dispose();
   }
 
@@ -57,101 +58,157 @@ class _RegisterViewState extends State<RegisterView> {
           child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                ...[TextFormField(
-                  controller: _email,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter a username';
-                    }
-                    RegExp emailValidator = RegExp(
-                      r'^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$',
-                    );
-                    if (!emailValidator.hasMatch(value)) {
-                      return 'Please enter a valid email address';
-                    }
-
-                    return null;
-                  },
-                  autofocus: true,
-                  textInputAction: TextInputAction.next,
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: const InputDecoration(
-                    filled: true,
-                    hintText: 'Your email address',
-                    labelText: 'Email',
-                  ),
-                ),
-                  TextFormField(
-                    controller: _password,
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter a password';
-                      }
-
-                      return null;
-                    },
-                    keyboardType: TextInputType.visiblePassword,
-                    obscureText: _obscureText,
-                    decoration: InputDecoration(
-                      filled: true,
-                      hintText: 'Your password',
-                      labelText: 'Password',
-                      suffixIcon: IconButton(
-                        icon: _obscureText ? const Icon(Icons.visibility_off) : const Icon(Icons.remove_red_eye),
-                        onPressed: _togglePasswordVisibility,
-                      ),
-                    ),
-                  ),
-                  TextFormField(
-                    controller: _confirmPassword,
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter a password';
-                      }
-
-                      return null;
-                    },
-                    keyboardType: TextInputType.visiblePassword,
-                    obscureText: _obscureText,
-                    decoration: InputDecoration(
-                      filled: true,
-                      hintText: 'Confirm your password',
-                      labelText: 'Confirm Password',
-                      suffixIcon: IconButton(
-                        icon: _obscureText ? const Icon(Icons.visibility_off) : const Icon(Icons.remove_red_eye),
-                        onPressed: _togglePasswordVisibility,
-                      ),
-                    ),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {
-                      final username = _email.text;
-                      final password = _password.text;
-
-                      _formKey.currentState?.validate();
-                      // TODO: Show dialog with error message
-                      // _showDialog('Network Error \n Please check your network connection');
-
-                      print('$username, $password');
-                    },
-                    child: const Text('Register'),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const Text('Already have an account?'),
-                      TextButton(
-                        onPressed: () {
-                          Navigator.pushNamed(context, '/login');
-                        },
-                        child: const Text('Sign in'),
-                      ),
-                    ],
-                  ),
+                ...[emailFormField(),
+                  nameFormField(),
+                  birthdayFormField(),
+                  passwordFormField(),
+                  confirmPasswordFormField(),
+                  registerButton(),
+                  signIn(context),
                 ].expand((widget) => [widget, const SizedBox(height: 20.0)]),]
           ),
         ),
       ),    );
+  }
+
+  Row signIn(BuildContext context) {
+    return Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text('Already have an account?'),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/login');
+                      },
+                      child: const Text('Sign in'),
+                    ),
+                  ],
+                );
+  }
+
+  ElevatedButton registerButton() {
+    return ElevatedButton(
+                  onPressed: () {
+                    _formKey.currentState?.validate();
+                    _formKey.currentState?.save();
+                    final username = _email.text;
+                    final password = _password.text;
+                    final birthday = _birthday?.toUtc().toIso8601String();
+                    final name = _name.text;
+                    print(_formKey.currentState);
+                    // TODO: Show dialog with error message
+                    // _showDialog('Network Error \n Please check your network connection');
+
+                    print('$username, $password, $birthday, $name');
+                  },
+                  child: const Text('Register'),
+                );
+  }
+
+  TextFormField confirmPasswordFormField() {
+    return TextFormField(
+                  controller: _confirmPassword,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter a password';
+                    }
+
+                    if (value != _password.text) {
+                      return 'Passwords do not match';
+                    }
+
+                    return null;
+                  },
+                  keyboardType: TextInputType.visiblePassword,
+                  obscureText: _obscureText,
+                  decoration: InputDecoration(
+                    filled: true,
+                    hintText: 'Confirm your password',
+                    labelText: 'Confirm Password',
+                    suffixIcon: IconButton(
+                      icon: _obscureText ? const Icon(Icons.visibility_off) : const Icon(Icons.remove_red_eye),
+                      onPressed: _togglePasswordVisibility,
+                    ),
+                  ),
+                );
+  }
+
+  TextFormField passwordFormField() {
+    return TextFormField(
+                  controller: _password,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter a password';
+                    }
+
+                    return null;
+                  },
+                  keyboardType: TextInputType.visiblePassword,
+                  obscureText: _obscureText,
+                  decoration: InputDecoration(
+                    filled: true,
+                    hintText: 'Your password',
+                    labelText: 'Password',
+                    suffixIcon: IconButton(
+                      icon: _obscureText ? const Icon(Icons.visibility_off) : const Icon(Icons.remove_red_eye),
+                      onPressed: _togglePasswordVisibility,
+                    ),
+                  ),
+                );
+  }
+
+  InputDatePickerFormField birthdayFormField() {
+    return InputDatePickerFormField(
+
+                  onDateSaved: (value) => _birthday = value,
+                    firstDate: DateTime.utc(1903), lastDate: DateTime.now());
+  }
+
+  TextFormField nameFormField() {
+    return TextFormField(
+                 controller: _name,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your name';
+                    }
+                    return null;
+                  },
+                  autofocus: true,
+                  textInputAction: TextInputAction.next,
+                  keyboardType: TextInputType.text,
+                  decoration: const InputDecoration(
+                    filled: true,
+                    hintText: 'Your name',
+                    labelText: 'Name',
+                  ),
+                );
+  }
+
+  TextFormField emailFormField() {
+    return TextFormField(
+                controller: _email,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter a valid email';
+                  }
+                  RegExp emailValidator = RegExp(
+                    r'^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$',
+                  );
+                  if (!emailValidator.hasMatch(value)) {
+                    return 'Please enter a valid email address';
+                  }
+
+                  return null;
+                },
+                autofocus: true,
+                textInputAction: TextInputAction.next,
+                keyboardType: TextInputType.emailAddress,
+                decoration: const InputDecoration(
+                  filled: true,
+                  hintText: 'Your email address',
+                  labelText: 'Email',
+                ),
+              );
   }
   void _showDialog(String message) {
     showDialog<void>(

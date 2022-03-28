a3fea5b9b1fad2ad711b18819bcda6ff8ec30624
Neil Ryan L <neilryanlipaod@gmail.com>
2022-03-28 21:26:46 +0800

    Modify dating_profile model
    Added gender field

--- lib/models/dating_profile.dart
+++ lib/models/dating_profile.dart
@@ -1,21 +1,26 @@
 
 import 'celebrity_profile.dart';
+import 'gender.dart';
 
 class DatingProfile {
+  String userId;
   String imageUrl;
   String name;
-  String age;
+  DateTime birthDay;
   String aboutMe;
   List<CelebrityProfile> celebrityLookAlikes;
+  Gender gender;
 
-  DatingProfile({required this.imageUrl, required this.name, required this.age, required this.aboutMe, required this.celebrityLookAlikes});
+  DatingProfile({required this.userId, required this.imageUrl, required this.name, required this.birthDay, required this.aboutMe, required this.celebrityLookAlikes, required this.gender});
 
   factory DatingProfile.fromJson(Map<String, dynamic> json) => DatingProfile(
+    userId: json["userId"],
     imageUrl: json["imageUrl"],
     name: json["name"],
-    age: json["age"],
+    birthDay: DateTime.parse(json["birthDay"]),
     aboutMe: json["aboutMe"],
-    celebrityLookAlikes: List<CelebrityProfile>.from(json["celebrityLookAlikes"].map((x) => CelebrityProfile.fromJson(x))),
+    celebrityLookAlikes: List<CelebrityProfile>.from(json["celebrityLookAlikes"].map((celeb) => CelebrityProfile.fromJson(celeb))),
+    gender: json["gender"] as Gender
   );
 
 }
---
+++ lib/models/models.dart
@@ -0,0 +1,1 @@
+export 'dating_profile.dart';

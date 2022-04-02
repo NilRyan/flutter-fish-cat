f8e942505f3e7027d0869066a26d0fd96baa0fb4
Neil Ryan L <neilryanlipaod@gmail.com>
2022-04-02 20:18:55 +0800

    Add mock intro screens

--- README.md
+++ README.md
@@ -4,3 +4,10 @@ A dating app with a twist
 
 ## Features
 ![img.png](design-materials/img.png)
+
+
+
+
+## Design Assets & Materials
+<a href={"https://www.streamlinehq.com"}>Free Selfie PNG illustration by Streamline</a>
+<a href={"https://www.streamlinehq.com"}>Free Searching Network PNG illustration by Streamline</a>
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:introduction_screen/introduction_screen.dart';
 
 import 'views/login_view.dart';
 import 'views/main_view.dart';
@@ -20,7 +21,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.pink,
 
       ),
-      home: const LoginView(),
+      home: IntroView(),
       routes: {
         '/login': (context) => const LoginView(),
         '/register': (context) => const RegisterView(),
@@ -29,6 +30,62 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+class IntroView extends StatelessWidget {
+  IntroView({Key? key}) : super(key: key);
+
+  void navigateToLogin(BuildContext context) {
+    Navigator.pushNamed(context, '/login');
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return IntroductionScreen(
+      pages: [
+        PageViewModel(
+          title: "Find your soul mate",
+          body: "Join us and find someone",
+          image: const Padding(
+            padding: EdgeInsets.only(top: 150.0),
+            child: Center(child: Image(image: AssetImage('assets/images/streamline-icon-selfie.PNG'))),
+          ),
+        ),
+        PageViewModel(
+          title: "FishCat",
+          body: "You can find people as handsome as Boss Philip John",
+          image: const Padding(
+            padding: EdgeInsets.only(top: 150.0),
+            child: Center(
+              child: Image(image: AssetImage('assets/images/streamline-icon-searching-network.PNG')),
+            ),
+          ),
+          footer: ElevatedButton(
+            onPressed: () => navigateToLogin(context),
+            child: const Text("Start FishCat Journey"),
+          ),
+        ),
+      ],
+      onDone: () => navigateToLogin(context),
+      showBackButton: false,
+      showNextButton: false,
+      showSkipButton: false,
+      skip: const Icon(Icons.skip_next),
+      done: const Text("Done", style: TextStyle(fontWeight: FontWeight.w600)),
+      dotsDecorator: DotsDecorator(
+          size: const Size.square(10.0),
+          activeSize: const Size(10.0, 10.0),
+          activeColor: Colors.pink,
+          color: Colors.black26,
+          spacing: const EdgeInsets.symmetric(horizontal: 3.0),
+          activeShape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(25.0)
+          )
+      ),
+    );
+  }
+}
+
 
 
 
